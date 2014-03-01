c6b180fc4dca5cb2fe85d626828668af5d948d17
Angelica Chen <angie@nat-oitwireless-inside-vapornet100-b-12181.princeton.edu>
2014-02-28 23:49:44 -0500

    Updated home page

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset = "utf-8">
+	<meta name="A non-profit that aims to spread literacy and compassion by connecting communities through the power of storytelling." content = "Read Record Replay">
+	<title>Read Record Replay</title>
+	<link href="style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+	<?php echo("Hello"); ?>
+	<div class = "menu">
+		<ul>
+			<li><a>Events</a>
+			<li><a>About</a>
+				<ul>
+					<li><a>Our Team</a></li>
+					<li><a>Mission</a></li>
+				</ul>
+			</li>
+			<li><a>Contact Us</a></li>
+		</ul>
+	</div>
+</body>
+</html>
